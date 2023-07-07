3bba33a47ae69de5937162dc281f58d67cea6e3e
Bluebird2000 <lateefahmad3868@gmail.com>
2023-07-07 13:57:32 +0100

    prep to deployment

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9fb09167ba798cc9"; }
+/******/ 		__webpack_require__.h = function() { return "02571edb6e9ddc75"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.d1ab7b2f58c99666.hot-update.js
+++ .next/static/webpack/webpack.d1ab7b2f58c99666.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "9fb09167ba798cc9"; }
+/******/ 	__webpack_require__.h = function() { return "b4bfc5270f5070a8"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
